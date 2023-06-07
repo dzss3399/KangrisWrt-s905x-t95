cdff372be433e33f70f09c978eec3b582c39f47d
Biangkerok32 <48493593+Biangkerok32@users.noreply.github.com>
2023-06-07 19:49:36 +0700

    Update diy-part2.sh

--- router-config/immortalwrt-21.02/diy-part2.sh
+++ router-config/immortalwrt-21.02/diy-part2.sh
@@ -143,10 +143,10 @@ sed -i 's/10.*/10.* 11.* 192.168.* 172.16.* 172.17.* 172.18.* 172.19.* 172.20.* 
 svn co https://github.com/karnadii/rooter/trunk/package/rooter-builds/0protocols/luci-proto-3x package/luci-proto-3x
 svn co https://github.com/karnadii/rooter/trunk/package/rooter-builds/0protocols/luci-proto-mbim package/luci-proto-mbim
 svn co https://github.com/karnadii/rooter/trunk/package/rooter/0drivers/rmbim package/rmbim
-svn co https://github.com/Mas-Er/X-ROOterSource2102/trunk/package/rooter/0drivers/rqmi package/rqmi
+#svn co https://github.com/Mas-Er/X-ROOterSource2102/trunk/package/rooter/0drivers/rqmi package/rqmi
 svn co https://github.com/karnadii/rooter/trunk/package/rooter/0basicsupport/ext-sms package/ext-sms
 svn co https://github.com/karnadii/rooter/trunk/package/rooter/0basicsupport/ext-buttons package/ext-buttons
-svn co https://github.com/Mas-Er/X-ROOterSource2102/trunk/package/rooter/ext-rooter-basic package/ext-rooter-basic
+#svn co https://github.com/Mas-Er/X-ROOterSource2102/trunk/package/rooter/ext-rooter-basic package/ext-rooter-basic
 # Rooter splash
 svn co https://github.com/karnadii/rooter/trunk/package/rooter/0splash/status package/status
 svn co https://github.com/karnadii/rooter/trunk/package/rooter/0splash/splash package/splash
