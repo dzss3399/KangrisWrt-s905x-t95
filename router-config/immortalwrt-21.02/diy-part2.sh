31e916c78572c3454ab74b6046806865ac96053f
Biangkerok32 <48493593+Biangkerok32@users.noreply.github.com>
2023-06-07 12:02:48 +0700

    Update diy-part2.sh

--- router-config/immortalwrt-21.02/diy-part2.sh
+++ router-config/immortalwrt-21.02/diy-part2.sh
@@ -13,30 +13,30 @@
 #================================
 # Set etc/openwrt_release
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
-echo "DISTRIB_SOURCECODE='KarnadiWrt'" >>package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCECODE='KangrisWrt'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-argon）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i '27 i\uci uci set luci.main.mediaurlbase="/luci-static-argon/"\nuci commit luci\n' package/emortal/default-settings/files/99-default-settings
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-sed -i 's/192.168.1.1/11.11.1.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/11.11.1.1/g' package/base-files/files/bin/config_generate
 
 # Set banner
-rm -rf ./package/emortal/default-settings/files/openwrt_banner
-svn export https://github.com/karnadii/KarnadiWrt/trunk/amlogic-s9xxx/common-files/rootfs/etc/banner package/emortal/default-settings/files/openwrt_banner
+#rm -rf ./package/emortal/default-settings/files/openwrt_banner
+# svn export https://github.com/karnadii/KarnadiWrt/trunk/amlogic-s9xxx/common-files/rootfs/etc/banner package/emortal/default-settings/files/openwrt_banner
 
 # Add the default password for the 'root' user（Change the empty password to 'password'）
-sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+# sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Set ssid
-sed -i "s/ImmortalWrt/KarnadiWrt/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i "s/ImmortalWrt/KangrisWrt/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # Set timezone
 sed -i -e "s/CST-8/WIB-7/g" -e "s/Shanghai/Jakarta/g" package/emortal/default-settings/files/99-default-settings-chinese
 
 # Set hostname
-sed -i "s/ImmortalWrt/KarnadiWrt/g" package/base-files/files/bin/config_generate
+sed -i "s/ImmortalWrt/KangrisWrt/g" package/base-files/files/bin/config_generate
 
 # Set Interface
 sed -i "9 i\uci set network.wana=interface\nuci set network.wana.proto='3g'\nuci set network.wana.device='/dev/ttyUSB1'\nuci set network.wana.service='LTE'\nuci set network.wana.apn='internet'\nuci set network.wana.ipv6='auto'\nuci set network.wanb=interface\nuci set network.wanb.proto='dhcp'\nuci set network.wanb.device='eth0.10'\nuci set network.wanc=interface\nuci set network.wanc.proto='dhcp'\nuci set network.wanc.device='usb0'\nuci set network.@device[0].ports='eth0' 'eth0.100' 'eth0.200' 'eth0.300'\nuci commit network" package/emortal/default-settings/files/99-default-settings
