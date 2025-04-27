0efd8c7d36d8e3e076f093fb4bad183f17accf30
ddwx3399 <pcj3399@hotmail.com>
2025-04-27 20:24:07 +0800

    Update diy-part2.sh

--- router-config/immortalwrt-21.02/diy-part2.sh
+++ router-config/immortalwrt-21.02/diy-part2.sh
@@ -20,7 +20,7 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 sed -i '27 i\uci uci set luci.main.mediaurlbase="/luci-static-argon/"\nuci commit luci\n' package/emortal/default-settings/files/99-default-settings
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-sed -i 's/192.168.1.1/11.11.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
 
 # Set banner
 rm -rf ./package/emortal/default-settings/files/openwrt_banner
