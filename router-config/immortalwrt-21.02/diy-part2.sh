5ed2694840daf93fa1e27703d3b03540eab71dd4
dzss3399 <182021396@qq.com>
2025-04-28 17:14:41 +0800

    Update diy-part2.sh

--- router-config/immortalwrt-21.02/diy-part2.sh
+++ router-config/immortalwrt-21.02/diy-part2.sh
@@ -98,9 +98,9 @@ chmod +x files/bin/speedtest
 # Add luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-pushd package/luci-app-openclash/tools/po2lmo
-make && sudo make install
-popd
+# pushd package/luci-app-openclash/tools/po2lmo
+# make && sudo make install
+# popd
 
 # Set clash-core
 mkdir -p files/etc/openclash/core
